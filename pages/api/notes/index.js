9902a4825bbe8f68f9d7a3ac87a28520975bc675
Rafly Ashifa <raflyashifa10@gmail.com>
2022-02-15 10:50:46 +0700

    make it better

--- pages/api/notes/index.js
+++ pages/api/notes/index.js
@@ -5,14 +5,13 @@ import cors from '../cors';
 connectDB();
 
 export default async (req, res) => {
-    cors(req, res);
     const { method } = req;
 
     switch (method) {
         case 'GET':
             try {
                 const notes = await Note.find({});
-
+                cors(req, res);
                 res.status(200).json({ success: true, data: notes })
             } catch (error) {
                 res.status(400).json({ success: false });
@@ -21,8 +20,8 @@ export default async (req, res) => {
         case 'POST':
             try {
                 const data = (req.body)
-                // console.log(data.title);
                 const note = await Note.create(data);
+                cors(req, res);
                 res.status(201).json({ success: true, data: note })
             } catch (error) {
                 console.log(error);
