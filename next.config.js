e66b43574810cafe944afa73189702bb0c7d2d8c
Rafly Ashifa <raflyashifa10@gmail.com>
2022-02-16 17:25:16 +0700

    bismillah

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    "BASE_URL": "https://stok-new.herokuapp.com",
+    "BASE_URL": "https://ash-suzaku.vercel.app",
     "MongoDB": "mongodb+srv://ash05:katon000@ash.jmpnc.mongodb.net/ASH?retryWrites=true&w=majority",
   }
 }
