057919a99ff74e4dca6d1b064ab91e52de6e3120
Rafly Ashifa <raflyashifa10@gmail.com>
2022-02-17 16:33:32 +0700

    bismillah

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    "BASE_URL": "https://ash-suzaku.vercel.app",
+    "BASE_URL": "https://district-19.vercel.app",
     "MongoDB": "mongodb+srv://ash05:katon000@ash.jmpnc.mongodb.net/ASH?retryWrites=true&w=majority",
   }
 }
