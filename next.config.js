a0457e5dd2a17bb947020ecb8ce4eecf8ae0a865
ashrouge <94835643+ashrouge@users.noreply.github.com>
2025-01-14 19:38:51 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    "BASE_URL": "https://district-19.vercel.app/",
+    "BASE_URL": "https://district-19.vercel.app",
     "MongoDB": "mongodb+srv://raflyashifa10:051001ASH100150@cluster0.hfw8s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   }
 }
