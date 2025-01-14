265a3daa5e62fd769d6c7bc7e65f91c48a7e44d4
ashrouge <raflyputra0510@gmail.com>
2025-01-14 19:19:08 +0700

    new update

--- pages/index.js
+++ pages/index.js
@@ -36,50 +36,6 @@ const Index = ({ notes }) => {
         })}
       </div>
     </div>
-    // <div className="h-screen font-sans" style={{ backgroundImage: "url(./images/1.jpeg)" }}>
-    //   <div className="container mx-auto h-full flex justify-center items-center">
-    //     <div className="">
-    //       <div className="border-teal bg-white p-8 mb-6 rounded-lg">
-    //         <h1 className="font-hairline mb-4 text-center text-2xl font-bold">
-    //           Login
-    //         </h1>
-    //         <div className="mb-4 form control w-96">
-    //           <label className="font-bold block mb-2 label">
-    //             <span className="label-text">Email</span>
-    //           </label>
-    //           <input
-    //             type="text"
-    //             placeholder="Email"
-    //             className="input input-accent input-bordered w-full"
-    //           />
-    //         </div>
-    //         <div className="mb-2 form control w-96">
-    //           <label className="font-bold block mb-2 label">
-    //             <span className="label-text">Password</span>
-    //           </label>
-    //           <input
-    //             type="text"
-    //             placeholder="Your Password"
-    //             className="input input-accent input-bordered w-full"
-    //           />
-    //         </div>
-    //         <p className="flex justify-start text-lit mb-4">
-    //           Don't have an account?{" "}
-    //           <a href="#" className="text-gray-700 underline mr-2">
-    //             Create an Account
-    //           </a>
-    //           .
-    //         </p>
-    //         <div className="flex justify-center">
-    //           <button className="flex justify-center btn w-48 h-2 rounded-2xl">
-    //             Login
-    //           </button>
-    //         </div>
-    //       </div>
-    //       <div></div>
-    //     </div>
-    //   </div>
-    // </div>
   )
 }
 
